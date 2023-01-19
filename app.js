9985bd7b30c605a63248fc347ee4b67de51d7e7e
Rosebud <rkle408@gmail.com>
2023-01-19 13:34:06 -0800

    functional name, focus, saved to local storage, edited font family

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // DOM elements
 const time = document.getElementById('time');
 const greeting = document.getElementById('greeting');
-const name = document.getElementById('name');
+const user = document.getElementById('user');
 const focus = document.getElementById('focus');
 
 // Show Time
@@ -38,20 +38,71 @@ function setBgGreet() {
     if(hour < 12) {
         //Morning
         document.body.style.backgroundImage = "url('../assets/morning.jpg')";
-        greeting.textContent = 'Good Morning';
+        greeting.textContent = 'Good Morning,';
     } else if(hour < 18) {
         // Afternoon
         document.body.style.backgroundImage = "url('../assets/afternoon.jpg')";
-        greeting.textContent = 'Good Afternoon';
+        greeting.textContent = 'Good Afternoon,';
     } else {
         //Evening
         document.body.style.backgroundImage = "url('../assets/evening.jpg')";
-        greeting.textContent = 'Good Evening';
+        greeting.textContent = 'Good Evening,';
         // Text will be hard to read on this dark bg, so change text color
         document.body.style.color = 'white';
     }
 }
 
+// Get user
+function getUser() {
+    if(localStorage.getItem('user') === null ) {
+        user.textContent = '[Enter user]';
+    } else {
+        user.textContent = localStorage.getItem('user');
+    }
+}
+
+// Set user
+function setUser(e) {
+    // Keypress on enter only (should not return next line either), and on blur (aka click anywhere else on screen)
+    if(e.which == 13 || e.type === 'keypress') {
+        if(e.keyCode == 13) { // Enter button is #13
+            localStorage.setItem('user', e.target.innerText);
+            user.blur();
+        }
+    } else { // blur
+        localStorage.setItem('user', e.target.innerText);
+    }
+}
+
+// Get Focus
+function getFocus() {
+    if(localStorage.getItem('focus') === null ) {
+        focus.textContent = '[Enter focus]';
+    } else {
+        focus.textContent = localStorage.getItem('focus');
+    }
+}
+
+// Set focus
+function setFocus(e) {
+    // Keypress on enter only (should not return next line either), and on blur (aka click anywhere else on screen)
+    if(e.type === 'keypress') {
+        if(e.which == 13 || e.keyCode == 13) { // Enter button is #13
+            localStorage.setItem('focus', e.target.innerText);
+            focus.blur();
+        }
+    } else { // blur
+        localStorage.setItem('focus', e.target.innerText);
+    }
+}
+
+user.addEventListener('keypress', setUser);
+user.addEventListener('blur', setUser);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
+
 // Run
 showTime();
 setBgGreet();
+getUser();
+getFocus();
